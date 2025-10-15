4805a61c0f99bf4cc49d5c832fc6b11917adbb5b
Lukas Picek <lukaspicek@gmail.com>
2025-10-15 12:52:45 +0200

    fix:import errot

--- dataset/fungi.py
+++ dataset/fungi.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import torch
 import torchvision.transforms as T
-from image_dataset import ImageDataset
+from dataset.image_dataset import ImageDataset
 from matplotlib import style
 
 style.use("seaborn-v0_8-whitegrid")
